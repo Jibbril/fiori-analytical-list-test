7deb9a20807bfd6991f436b2daf90db8c6ef371c
Jibbril Berbres <jibbril.berbres@viego.se>
2023-05-19 11:16:49 +0200

    Generate regular LROP

---
+++ app/regular-lrop/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,26 @@
+sap.ui.require(
+    [
+        'sap/fe/test/JourneyRunner',
+        'regularlrop/test/integration/FirstJourney',
+		'regularlrop/test/integration/pages/BooksList',
+		'regularlrop/test/integration/pages/BooksObjectPage'
+    ],
+    function(JourneyRunner, opaJourney, BooksList, BooksObjectPage) {
+        'use strict';
+        var JourneyRunner = new JourneyRunner({
+            // start index.html in web folder
+            launchUrl: sap.ui.require.toUrl('regularlrop') + '/index.html'
+        });
+
+       
+        JourneyRunner.run(
+            {
+                pages: { 
+					onTheBooksList: BooksList,
+					onTheBooksObjectPage: BooksObjectPage
+                }
+            },
+            opaJourney.run
+        );
+    }
+);
--- app/services.cds
+++ app/services.cds
@@ -2,3 +2,5 @@
 using from './list-report-app/annotations';
 
 using from './foldable-groups/annotations';
+
+using from './regular-lrop/annotations';
